acc8bc68f3711a0c8f1c03dc6cbd293faa5fc215
Jean Christophe Meunier, PhD <jmeunier@paradigm.brussels>
2025-05-21 11:19:31 +0200

    feat: add setup files to repo

---
+++ my_statements.py
@@ -0,0 +1,16 @@
+my_list = ["appel", "cherry", "lemon"]
+my_dict = {"drink": "coffe", "milk": "whole"}
+           
+for item in my_list:
+    print(f"My favorite thing to eat is : {item}")
+
+for _, value in my_dict.items():
+    print(f"My favorite thingh to drink is : {value}")
+
+
+# my_dict = my_dict
+# my_list = my_list
+
+
+
+
