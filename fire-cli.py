182aab44ea6442b709f8f2d306138705961e3fed
Jean Christophe Meunier, PhD <jmeunier@paradigm.brussels>
2025-05-22 10:17:33 +0200

    feat: add fire example for facilitation CLI usage

---
+++ fire-cli.py
@@ -0,0 +1,17 @@
+import fire
+from mylib.bot import scrape
+
+# This script is a command line interface (CLI) for the scrape function.
+# It allows users to scrape the first sentence of a Wikipedia page using command line arguments.
+if __name__ == '__main__':
+  """
+  This script is a command line interface (CLI) for the scrape function.
+  It allows users to scrape the first sentence of a Wikipedia page using command line arguments.
+  """
+  fire.Fire(scrape)
+
+# Usage in CLI:
+# python fire-cli.py --name Microsoft --lenght 1
+# python fire-cli.py --name Microsoft
+# python fire-cli.py --name Microsoft --lenght 2
+# python fire-cli.py --name Microsoft --lenght 3
